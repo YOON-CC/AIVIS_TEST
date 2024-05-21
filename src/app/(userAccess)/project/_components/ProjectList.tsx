36ffc1519efd55692af897cf56e97c97f2a807e7
YOON-CC <whdbscks77@gmail.com>
2024-05-21 12:06:47 +0900

    feat: 게시글 리스트 구현 (#3)

--- src/app/(userAccess)/project/_components/ProjectList.tsx
+++ src/app/(userAccess)/project/_components/ProjectList.tsx
@@ -1,11 +1,23 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
+
+interface Project {
+  numberOfImages: ReactNode;
+  id: number;
+  name: string;
+  ontologyName: string;
+  created: string;
+  updated: string;
+  status: string;
+}
 
 export default function ProjectList() {
   const [token, setToken] = useState<string | null>(null);
   const [shortTermToken, setShortTermToken] = useState<string | null>(null);
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<Project[]>([]);
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+  const [showDetails, setShowDetails] = useState<boolean>(false); // 세부 정보를 표시할지 여부를 나타내는 상태
 
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
@@ -37,7 +49,8 @@ export default function ProjectList() {
 
           if (response.ok) {
             const data = await response.json();
-            setData(data);
+            setData(data.collection);
+            console.log(data.collection[0].name);
             console.log(data);
           } else {
             console.error("Failed to fetch data");
@@ -51,6 +64,13 @@ export default function ProjectList() {
     fetchData();
   }, [token]);
 
+  const handleProjectClick = (project: Project) => {
+    setSelectedProject(project);
+  };
+  const closeModal = () => {
+    setSelectedProject(null); // 선택된 프로젝트 초기화
+  };
+
   return (
     <main>
       <div>
@@ -61,15 +81,76 @@ export default function ProjectList() {
           style={{
             display: "flex",
             width: "100%",
-            justifyContent: "space-between",
+            justifyContent: "space-evenly",
+            background: "navy",
+            color: "white",
+            borderRadius: "5px",
           }}
         >
+          <p> </p>
           <p>name</p>
           <p>number of images</p>
           <p>created</p>
         </div>
-        {/* {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>} */}
+        {data.length > 0 ? (
+          <div>
+            {data.map((project) => (
+              <div
+                key={project.id}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  cursor: "pointer",
+                  background:
+                    selectedProject?.id === project.id ? "#e0e0e0" : "white",
+                }}
+              >
+                <h4 onClick={() => handleProjectClick(project)}>
+                  ▶상세입니다.
+                </h4>
+                <h4>{project.name}</h4>
+                <h4>{project.numberOfImages}</h4>
+                <h4>{new Date(Number(project.created)).toLocaleString()}</h4>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
+      {selectedProject && (
+        <div
+          style={{
+            background: "black",
+            marginTop: "20px",
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "10px",
+            color: "white",
+            padding: "20px",
+          }}
+        >
+          <span
+            className="close"
+            onClick={closeModal}
+            style={{ fontSize: "30px", right: "-0" }}
+          >
+            &times;
+          </span>{" "}
+          <p>
+            <strong>Name:</strong> {selectedProject.id}
+          </p>
+          <p>
+            <strong>Ontology Name:</strong> {selectedProject.ontologyName}
+          </p>
+          <p>
+            <strong>Created:</strong>{" "}
+            {new Date(Number(selectedProject.created)).toLocaleString()}
+          </p>
+        </div>
+      )}
     </main>
   );
 }
